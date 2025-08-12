1375b81618456acb8e2112f78f5677e5d81236a1
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-08-12 18:33:34 +0530

    Add Array package

---
+++ src/Array/startingArray.java
@@ -0,0 +1,9 @@
+package Array;
+
+public class startingArray {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("hello maths");
+        System.out.println("hyy");
+    }
+}

38a575a0b4860090fd3d4ad4d675c05c3d8958ee
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-08-17 21:11:18 +0530

    add some extra lines of code

--- src/Array/initialization.java
+++ src/Array/initialization.java
@@ -3,6 +3,9 @@ package Array;
 public class initialization {
     public static void main(String[] args) {
         int[] arr = {2,3,4,5,6};
-        System.out.println(arr[3]);
+        int a = arr.length;
+        for(int i = 0; i<a; i++) {
+            System.out.print(arr[i]+" ");
+        }
     }
 }

950205a4e8cabd87b5a3abd6294172deed9a3a37
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-08-22 01:33:11 +0530

    new question added

---
+++ src/Array/Doublet.java
@@ -0,0 +1,21 @@
+/*
+Ques: Find the doublet in the array whose sum is equal
+to the given value x.(Two sum)
+ */
+package Array;
+
+public class Doublet {
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,6,7,8,9};
+        int x = 13;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                int a = 0;
+                if(arr[i]+arr[j]==x){
+                    a = arr[i]+arr[j];
+                    System.out.println(arr[i]+" + "+arr[j]+" = "+a);
+                }
+            }
+        }
+    }
+}

9b62ff87cef91e89f42b938432bd945724232ef5
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-08-24 23:08:57 +0530

    new commit

---
+++ src/Array/RotateTheArray.java
@@ -0,0 +1,11 @@
+/*
+Ques: Rotate the given array 'a' by k steps, where is non-negative.
+Note: k can be greater than n as well where n is the size of the array 'a'.
+ */
+package Array;
+
+public class RotateTheArray {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Methods/Understanding.java
+++
@@ -1,7 +0,0 @@
-package Methods;
-
-public class Understanding {
-    public static void main(String[] args) {
-        System.out.println(Math.max(3,4));
-    }
-}

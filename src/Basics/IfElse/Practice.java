81fa6f661d0a1c980232e97551cfc22eee7a0a42
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-08-25 22:06:50 +0530

    if else

---
+++ src/Basics/IfElse/Practice.java
@@ -0,0 +1,14 @@
+package Basics.IfElse;
+
+public class Practice {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        if(a+b>10){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+    }
+}

4ab5292f490221b6095f03305781e2a9dd43364c
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-08-29 00:31:58 +0530

    String methods

---
+++ src/Methods/StringMethod.java
@@ -0,0 +1,19 @@
+/*
+Ques: Use String value in methods.
+ */
+package Methods;
+
+import java.util.Scanner;
+
+public class StringMethod {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.next();
+        System.out.print(greet(name));
+    }
+    static String greet(String name){
+        String greeting = "Good morning "+name;
+        return greeting;
+    }
+}

29bacb280a96f6fedd221296c60fa06831868d8e
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-08-29 01:48:36 +0530

    Swap numbers using method

---
+++ src/Methods/Swap.java
@@ -0,0 +1,23 @@
+/*
+Ques: Write a program to show swaping of two number
+using method is possible or not.
+ */
+package Methods;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a = 10 , b = 20;
+        System.out.println(a+" "+b);// #Output: 10 20
+        swap(a,b);// swap(10,20)
+        System.out.println(a+" "+b);// #Output: 10 20
+    }
+    public static void swap(int a, int b){//int a = 10, b = 20
+        int temp = a;
+        a = b;
+        b = temp;
+        // a = 20
+        // b = 10
+        // But this is a void so it will not return any value
+    }
+}
+

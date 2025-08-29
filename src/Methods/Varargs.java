e0077f03388db96e7df6a2448af07ebdd79ce26c
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-08-29 15:24:15 +0530

    varargs

--- src/Methods/Varargs.java
+++ src/Methods/Varargs.java
@@ -8,6 +8,10 @@ import java.util.Arrays;
 public class Varargs {
     public static void main(String[] args) {
         fun(2,3,5,6,7,4,9);
+        multiple(5, 8,"Prashant","Sahil","Asif");
+    }
+    static void multiple(int a, int b, String ...v){
+        System.out.println(a+" "+b+" "+Arrays.toString(v));
     }
     static void fun(int ...v){
         System.out.println(Arrays.toString(v));

4dedc94d337eed6116943937fcadc6082b4169b6
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-08-29 14:45:12 +0530

    Shadowing

---
+++ src/Methods/Shadowing.java
@@ -0,0 +1,19 @@
+/*
+Topic: Shadowing (If anything is initialized before psvm then
+                  it becomes parent of everything which comes
+                  under that)
+ */
+package Methods;
+
+public class Shadowing {
+    static int x = 10; // this will shadow on line 7.
+    public static void main(String[] args) {
+        System.out.println(x); // x = 10
+        int x = 20;
+        System.out.println(x); // x = 20
+        fun(); // x = 10
+    }
+    static void fun(){
+        System.out.println(x); // x = 10
+    }
+}

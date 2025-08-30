d3d17bb285ff3877c5b33650634bde070d098f3c
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-08-30 23:04:16 +0530

    new repo added

--- src/Methods/Armstrong.java
+++ src/Methods/Armstrong.java
@@ -6,11 +6,10 @@
                   System.out.print("There are all "+i+" ");
                }
      }
- */
+  */
 package Methods;
 
 import java.util.Scanner;
-
 public class Armstrong {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);

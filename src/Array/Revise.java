3a368d6635d4704426762adb4afdcc6fcadb17f5
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-10-05 00:17:57 +0530

    updated

--- src/Array/Revise.java
+++ src/Array/Revise.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class Revise {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Array size: ");
+        System.out.print("Enter Array size: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
 
-        System.out.println("Enter Array elements: ");
+        System.out.print("Enter Array elements: ");
         for(int i=0; i<arr.length;i++){
             arr[i] = sc.nextInt();
         }
-        System.out.println("You Entered: ");
+        System.out.print("You Entered: ");
         for(int i=0; i<n;i++){
-            System.out.println(arr[i]+" ");
+            System.out.print(arr[i]+" ");
         }
     }
 }

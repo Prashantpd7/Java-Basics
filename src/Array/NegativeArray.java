5d07ec9bde402e407423295e8d15c292f4782de6
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-10-05 23:00:49 +0530

    new question

---
+++ src/Array/NegativeArray.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class NegativeArray {
+    public static void main(String[] args) {
+        int[] arr = {2,-4,6,-8,9,-5,1};
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]<0){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}

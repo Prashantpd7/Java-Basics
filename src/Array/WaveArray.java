5d2d98eba9a9876e506412c094efe25e6b08160b
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-10-12 01:57:45 +0530

    new question

--- src/Array/WaveArray.java
+++ src/Array/WaveArray.java
@@ -2,7 +2,7 @@ package Array;
 
 public class WaveArray {
     public static void main(String[] args) {
-        int [] arr  = {1,2,3,4,5};
+        int [] arr  = {1,2,3,4,5,6};
         for(int i=0; i<arr.length; i+=2){
             if(i==arr.length-1) break;
             int temp = arr[i];

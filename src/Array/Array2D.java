bc6c2c19043a504cf163a6483a9fcc4c643d2c5b
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-10-18 03:20:13 +0530

    new question

---
+++ src/Array/Array2D.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Array2D {
+    public static void main(String[] args) {
+        int [][] arr = {{2,3,4},{9,8},{9,7,6}};
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}

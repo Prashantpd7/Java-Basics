9027d8d15fd4581008e67d0235b7dd0d95f95e24
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-10-18 22:40:32 +0530

    new question

--- src/Array/Array2D.java
+++ src/Array/Array2D.java
@@ -4,7 +4,8 @@ public class Array2D {
     public static void main(String[] args) {
         int [][] arr = {{2,3,4},{9,8},{9,7,6}};
         for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr[i].length; j++){
+            for(int j=0; j<arr[i].length; j++)
+            {
                 System.out.print(arr[i][j]+" ");
             }
             System.out.println();

12e3ba9c1bc30ee68477e262d52b468dac5cab6f
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-10-24 22:08:39 +0530

    new quesiton

--- src/Array/LSearch.java
+++ src/Array/LSearch.java
@@ -7,9 +7,9 @@ public class LSearch {
         int ans = linearSearch(nums, target);
         System.out.println(ans);
     }
-
     static int linearSearch(int[] arr, int target) {
-        if (arr.length == 0) {
+        if (arr.length == 0)
+        {
             return -1;
         }
         for (int index = 0; index < arr.length; index++) {

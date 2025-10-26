daa03fa6a6b68370bc6059012ac4d63a8fb5e61e
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-10-26 22:43:24 +0530

    new quesiton

--- src/Array/ArrayLists.java
+++ src/Array/ArrayLists.java
@@ -26,7 +26,8 @@ public class ArrayLists {
         int size = list.size();
         System.out.println(size);
 
-        for(int i = 0; i<list.size(); i++){
+        for(int i = 0; i<list.size(); i++)
+        {
             System.out.println(list.get(i));
         }
         System.out.println();

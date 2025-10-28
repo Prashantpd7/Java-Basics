ab611a075b16461102c01a0bcc15a0e702f39d28
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-10-28 23:22:46 +0530

    new question

---
+++ src/Array/FunctionRevise.java
@@ -0,0 +1,14 @@
+package Array;
+
+import java.util.Scanner;
+
+public class FunctionRevise {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+    }
+    public static void printMyName(String name){
+
+    }
+
+}

4f0173478040433dcc42230f9a01bc3e666fb8de
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-10-28 23:32:21 +0530

    new question

--- src/Array/FunctionRevise.java
+++ src/Array/FunctionRevise.java
@@ -6,9 +6,10 @@ public class FunctionRevise {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
+        printMyName(name);
     }
     public static void printMyName(String name){
-
+        System.out.println(name);
     }
 
 }

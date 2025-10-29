f05e031b3a2fd4982ad84a29c459e8974f29d793
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-10-29 17:14:31 +0530

    new question

--- src/Array/FunctionRevise.java
+++ src/Array/FunctionRevise.java
@@ -7,9 +7,15 @@ public class FunctionRevise {
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
         printMyName(name);
+        int sum = totalspellings();
+        System.out.println(sum);
     }
     public static void printMyName(String name){
         System.out.println(name);
+    }
+    public static int totalspellings(){
+        int total = 8;
+        return total;
     }
 
 }

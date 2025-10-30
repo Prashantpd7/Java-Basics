9728c0323369fe0ad54901d70eacb535b6f241cd
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-10-30 23:20:45 +0530

    new question

--- src/Array/FunctionRevise.java
+++ src/Array/FunctionRevise.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class FunctionRevise {
     public static void main(String[] args) {
         int ans = Fill();
-        System.out.println("Sum is: "+ans);
+        System.out.println("Sum of these no. is: "+ans);
     }
     public static int Fill(){
         Scanner sc = new Scanner(System.in);

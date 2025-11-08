776a772d0c9f1aeccead1283d2babd05007795d3
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-11-08 23:09:09 +0530

    new question

--- src/Array/Initialization.java
+++ src/Array/Initialization.java
@@ -4,7 +4,8 @@ public class Initialization {
     public static void main(String[] args) {
         int[] arr = {2,3,4,5,6};
         int n = arr.length;
-        for(int i = 0; i<n; i++) {
+        for(int i = 0; i<n; i++)
+        {
             System.out.print(arr[i]+" ");
         }
     }

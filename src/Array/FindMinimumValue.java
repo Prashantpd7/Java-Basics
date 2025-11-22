b960bbb805cdc5323092b5cdc0d91c1382e881a9
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-11-22 21:40:59 +0530

    Update FindMinimumValue.java

--- src/Array/FindMinimumValue.java
+++ src/Array/FindMinimumValue.java
@@ -1,7 +1,7 @@
 package Array;
 public class FindMinimumValue {
     public static void main(String[] args) {
-        int[] arr = {3,5,-1,8,4,2};
+        int[] arr = {3,5,-1,9,8,4,2};
         int min = arr[0];
         for(int i=1; i<arr.length; i++){
             if(min>arr[i]) min=arr[i];

7a391694e4204870184ca1f8c6af7c1042d9441a
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-11-23 20:46:54 +0530

    Update CountTheNum.java

--- src/Array/CountTheNum.java
+++ src/Array/CountTheNum.java
@@ -4,7 +4,7 @@ Ques: Count the number of the        elements in the given array greater than a 
 package Array;
 public class CountTheNum {
     public static void main(String[] args){
-        int[] arr = {1,3,5,2,4,6,4,8,9,6,7};
+        int[] arr = {1,3,5,7,2,4,6,4,8,9,6,7};
         int x = 3;
         int count = 0;
         for(int i=0;i<arr.length;i++){

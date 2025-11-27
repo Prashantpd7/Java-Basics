038993ae2a67718ebedc4762969d79f11ebe856e
Prashant Dwivedi <prashantdwivedipd7@gmail.com>
2025-11-27 14:20:49 +0530

    Update MergeSorted.java

--- src/Array/MergeSorted.java
+++ src/Array/MergeSorted.java
@@ -6,7 +6,7 @@ package Array;
 public class MergeSorted {
     public static void main(String[] args) {
         int[] a = {2,5,6,9,20};
-        int[] b = {1,3,5,7,8};
+        int[] b = {1,3,5,7,9,8};
         int[] c = new int[a.length+b.length];
         for(int ele : c) System.out.print(ele+" ");
         System.out.println();
